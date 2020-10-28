0b6cbdfd2ec20d6a71ddfb67e7985063b3b36b40
Canadixn <70049844+Canadixn@users.noreply.github.com>
2020-10-28 17:47:45 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,9 +6,6 @@ module.exports.run = async (bot, message, args) => {
 
     const pingembed = new Discord.MessageEmbed()
     .setAuthor("Pong!", "https://cdn.discordapp.com/attachments/737385723703066706/759616004233101362/source_1.gif")
-    
-    .setColor("#000000")
-
     .addFields(
 
     { name: '⌛ Bot Latency:', value: `${msg.createdTimestamp - message.createdTimestamp}ms`, inline: true },
@@ -16,20 +13,18 @@ module.exports.run = async (bot, message, args) => {
     { name: '💓 API Latency:', value: `${Math.round(bot.ws.ping)}ms`, inline: true }
 
     )
-
     .setFooter(`ID: ${message.author.id} • Servers: ${bot.guilds.cache.size}`)
 
     .setTimestamp();
-
-
+    
     msg.edit(``, pingembed);
 
   }
 
-  
+
 
 module.exports.help = {
   name: "ping",
   description: "Pings CanvaCord.",
-  aliases: []
+  aliases: ["latency", "alexa_show_me_ping"]
 }
