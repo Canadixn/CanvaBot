d77dc258d7085218d85f0c02b33b6f85eb746ab8
Canadixn <70049844+Canadixn@users.noreply.github.com>
2020-10-28 17:45:43 -0600

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 var figlet = require('figlet');
 
 module.exports.run = async (bot, message, args) => {
@@ -10,22 +12,32 @@ msg = args.join(" ");
     verticalLayout: 'default',
     width: 80,
     whitespaceBreak: true
-}, function(err, data) {
+
+}, 
+
+function(err, data) {
+
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
-    message.channel.send(`\`\`\`${data}\`\`\``);
-});
 
+
+    const Embed = new Discord.MessageEmbed()
+    .setDescription(`\`\`\`${data}\`\`\``);
+
+    message.channel.send(Embed);
+
+if (Embed.description.length >= 2048)
+return message.channel.send("Your text is too long! Please shorten it.").catch(console.error);
+
+});
 
 }
 
 module.exports.help = {
-
 name: "ascii",
-aliases: ["texttoascii"],
-description: "Generates an ASCII Art from your text."
-
+aliases: ["asc"],
+description: "Prints ASCII Art."
 }
