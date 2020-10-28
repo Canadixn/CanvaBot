23a16661d90fb683bb425910263f3ce70a1e5ea2
Canadixn <70049844+Canadixn@users.noreply.github.com>
2020-10-28 17:42:25 -0600

    Update bed.js

--- commands/bed.js
+++ commands/bed.js
@@ -4,13 +4,9 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
     let m = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0] || message.author.id));
-    
     if(!m || !m.user || !m.user.displayAvatarURL()) m = message.member;
-    
     let av1 = message.author.displayAvatarURL({dynamic: false, format: "png"});
-    
     let av2 = m.user.displayAvatarURL({dynamic: false, format: "png"});
-
     let bim = await canvacord.Canvas.bed(av1, av2);
     let attachment = new Discord.MessageAttachment(bim, 'bed.png');
 
@@ -18,9 +14,8 @@ module.exports.run = async (bot, message, args) => {
 
 }
 
-
 module.exports.help = {
   name: "bed",
   aliases: ["b"],
-  description: "Pings CanvaCord."
+  description: "Bed Meme"
 }
