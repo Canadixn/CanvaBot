f1b4224808e32516ed979809762442211b769869
Canadixn <70049844+Canadixn@users.noreply.github.com>
2020-11-04 19:47:06 -0600

    Update christmas.js

--- commands/christmas.js
+++ commands/christmas.js
@@ -11,7 +11,7 @@ var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
 var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
 const embed = new Discord.MessageEmbed()
-.setAuthor('Christmas Countdown', "https://cdn.discordapp.com/attachments/770760785873469473/773724254322819092/p_5b4yrkYF7x_l_xOiyJzbPDZyOm8kwZ19RmkWXo4sWs1CMCxwVjDch3gO-7rAmNVbm2GHiIrj4w>
+.setAuthor('Christmas Countdown', "https://cdn.discordapp.com/attachments/770760785873469473/773724254322819092/p_5b4yrkYF7x_l_xOiyJzbPDZyOm8kwZ19RmkWXo4sWs1CMCxwVjDch3gO-7rAmNVbm2GHiIrj4w1zaFE0eDLCiSZRkVrIWmEiu1.png")
 .setDescription(`There are about **${days}** days and **${hours}** hours left until Christmas!`)
 .setTimestamp()
 message.channel.send(embed);
