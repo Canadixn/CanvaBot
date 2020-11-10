34227da9edf04119d31b1f532fe6910cef878976
Canadixn <70049844+Canadixn@users.noreply.github.com>
2020-11-10 13:46:42 -0600

    Create invert.js

---
+++ commands/invert.js
@@ -0,0 +1,21 @@
+const canvacord = require("canvacord");
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+
+    let m = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0] || message.author.id));
+    if(!m || !m.user || !m.user.displayAvatarURL()) m = message.member;
+    let avatar = m.user.displayAvatarURL({dynamic: false, format: "png"});
+
+    let image = await canvacord.Canvas.invert(avatar);
+    let attachment = new Discord.MessageAttachment(image, 'inverted.png');
+
+    message.channel.send(attachment);
+
+}
+
+module.exports.help = {
+  name: "invert",
+  aliases: ["curse", "hardoverlay"],
+  description: "Inverts your avatar."
+}
